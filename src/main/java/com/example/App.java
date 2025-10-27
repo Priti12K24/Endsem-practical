833f9918c57084e590dde1554eb967cdef98fda1
Priti12K24 <korakepriti2@gmail.com>
2025-10-27 11:51:20 +0530

    Added Maven project files

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package main.java.com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from endsem app!");
+        System.out.println("✅ Hello from Java CI/CD Pipeline!");
     }
 }
